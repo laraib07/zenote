7733bfab8f9a3886fc77ee987b837cdcc084495b
laraib07 <a.laraib@protonmail.com>
2023-09-16 11:30:37 +0530

    refactor: remove typeconverters

--- app/src/main/java/com/laraib07/zenote/data/NoteDatabase.kt
+++ app/src/main/java/com/laraib07/zenote/data/NoteDatabase.kt
@@ -2,16 +2,12 @@ package com.laraib07.zenote.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import com.laraib07.zenote.data.converter.LongListConverter
-import com.laraib07.zenote.data.converter.TodoListConverter
 import com.laraib07.zenote.data.dao.NoteDao
 import com.laraib07.zenote.data.dao.TagDao
 import com.laraib07.zenote.data.dao.TodoDao
 import com.laraib07.zenote.data.model.*
 
 
-@TypeConverters(TodoListConverter::class, LongListConverter::class)
 @Database(
     entities = [
         BaseNote::class,
